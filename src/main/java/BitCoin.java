3fffb9681a6d1e33dfd7446636b3a858e6a0e4cc
martenstromberg <marten.b.stromberg@gmail.com>
2018-03-06 19:32:57 +0100

    clean up branch on private mac

---
+++ src/main/java/BitCoin.java
@@ -0,0 +1,18 @@
+public class BitCoin {
+    private String base;
+    private String currency;
+    private String amount;
+
+    public String getBase () {
+        return base;
+    }
+
+    public String getCurrency () {
+        return currency;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+}
